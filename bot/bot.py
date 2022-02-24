f7ed548894df776c2eec616a3774660f898234bb
Edouard Eltherington <edouard.eltherington@gmail.com>
2022-02-24 11:46:04 -0700

    some file name changes

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,6 @@
 
 from contextlib import nullcontext
-from data_utils import load_data
+from naive_bot import load_data
 
 
 class bot:   
@@ -8,15 +8,15 @@ class bot:
         self.name = "Indiana Bones"
         self.dict = load_data("/keywords.csv")
 
-    def respond(self, str_input):
-        words = str_input.lower().split()
-        for word in words:
-            if word in self.dict.keys():
-                return self.dict[word];
-        return "I'm sorry, I don't understand.";    
+        def respond(self, str_input):
+            words = str_input.lower().split()
+            for word in words:
+                if word in self.dict.keys():
+                    return self.dict[word];
+            return "I'm sorry, I don't understand.";    
 
-    def test(self):
-        print("Hot Damn, this actually works!")
+        def test(self):
+            print("Hot Damn, this actually works!")
 
-    
+        
 
