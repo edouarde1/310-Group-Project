c7a97ff8ad1d6bb718e0e20725401162dcfa7572
Rylan Cox <rylanrcox@gmail.com>
2022-03-16 20:44:20 -0700

    impement detect_syn

---
+++ bot/syn_detection.py
@@ -0,0 +1,10 @@
+from nltk.corpus import wordnet
+
+def detect_syn(input):
+    synonyms = []
+    #This first loop iterates through all of the synsets that are associated with our input using wordnet.
+    for syn in wordnet.synsets(input):
+        #Then, also using word net it iterates through all of the words that are under that synset. We append that to a list and get our list of synonyms.
+        for i in syn.lemmas():
+            synonyms.append(i.name())
+    return synonyms
